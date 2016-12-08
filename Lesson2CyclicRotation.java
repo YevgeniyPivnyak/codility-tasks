35ec16daac8ea80b78989d6d12e6686a2ae44746
YevgeniyPivnyak <dneprojek@gmail.com>
2016-12-08 23:29:17 +0200

    modified the lesson 2

--- Lesson2CyclicRotation.java
+++ Lesson2CyclicRotation.java
@@ -4,36 +4,46 @@ public class Lesson2CyclicRotation {
     public static void main(String[] args) {
 
         int[] Array1 = new int[10];
+
         for (int i = 0; i < 10; i++) {
             Array1[i] = i;
         }
+        for (int i = 0; i < Array1.length; i++) {
+            System.out.print(Array1[i] + " ");
+        }System.out.println();
 
-        System.out.println(Array1[0] + " " + Array1[1] + " " + Array1[3]);
 
         shiftArray(Array1, 2);
-
 
     }
 
     public static int[] shiftArray(int[] Array1, int shift) {
 
         if (Array1 != null) {
-
-
             int length = Array1.length;
             int[] Array2 = new int[length];
 
-            System.out.println(Array2[0] + " " + Array2[1] + " " + Array2[3]);
+            for (int j = 0; j < Array2.length; j++) {
+                System.out.print(Array2[j] + " ");
+            }System.out.println();
+//
+//            System.arraycopy(Array1, shift, Array2, 0, length - shift);
+//
+//            System.arraycopy(Array1, 0, Array2, length - shift, shift);
 
-            System.arraycopy(Array1, shift, Array2, 0, length - shift);
+            for (int i = 0; i < Array1.length; i++) {
+                Array2[i+shift]=Array1[i];
+//                System.out.println(Array2[0] + " " + Array2[1] + " " + Array2[3]);
+            }
 
-
-            System.arraycopy(Array1, 0, Array2, length - shift, shift);
             return Array2;
+
 
         } else {
             return null;
+
         }
 
     }
+
 }
---
+++ Lesson2OddOccurrencesInArray.java
@@ -0,0 +1,49 @@
+
+public class Lesson2OddOccurrencesInArray {
+    public static void main(String[] args) {
+
+        int[] Array11 = new int[11];
+        int[] Array12 = new int[11];
+
+        Array11[0] = 1;
+        Array11[1] = 2;
+        Array11[2] = 1;
+        Array11[3] = 3;
+        Array11[4] = 4;
+        Array11[5] = 3;
+        Array11[6] = 4;
+        Array11[7] = 6;
+        Array11[8] = 5;
+        Array11[9] = 6;
+        Array11[10] = 1;
+
+        Array12 = Array11;
+
+        for (int i = 0; i < Array11.length; i++) {
+            System.out.print(Array11[i] + " ");
+        }
+        System.out.println();
+        for (int j = 0; j < Array12.length; j++) {
+            System.out.print(Array12[j] + " ");
+        }
+        unpairedArray(Array11, Array12);
+
+    }
+
+    public static int unpairedArray(int[] Array11, int[] Array12) {
+        System.out.println("////////////////////////////////////");
+        for (int i = 0; i < Array11.length; i++) {
+//            System.out.println(i);
+            int count = 0;
+
+            for (int j = 0; j < Array12.length; j++) {
+
+                System.out.println(i);
+                if (i == j) {count++; }
+            }if (count == 2) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
